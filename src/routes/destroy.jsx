3edbbd13a108a79beeaa0cf7a40d0e0c43dd9d4c
Nnamdi Okeke <onnamdialex@gmail.com>
2023-01-23 15:02:06 +0100

    :sparkles: Contextual errors

--- src/routes/destroy.jsx
+++ src/routes/destroy.jsx
@@ -2,6 +2,7 @@ import { redirect } from "react-router-dom";
 import { deleteContact } from "../contact";
 
 export async function action({ params }) {
+  throw new Error("Oh damn!");
   await deleteContact(params.contactId);
   return redirect("/");
 }
