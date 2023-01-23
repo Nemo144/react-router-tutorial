0d4000b53956b8442fbdbe6b35b0bfd36a6f36ce
Nnamdi Okeke <onnamdialex@gmail.com>
2023-01-23 16:09:15 +0100

    :zap: mutations without navigations

--- src/routes/contact.jsx
+++ src/routes/contact.jsx
@@ -1,5 +1,12 @@
-import { Form, useLoaderData } from "react-router-dom";
-import { getContact } from "../contact";
+import { Form, useLoaderData, useFetcher } from "react-router-dom";
+import { getContact, updateContact } from "../contact";
+
+export async function action({ request, params }) {
+  let formData = await request.formData();
+  return updateContact(params.contactId, {
+    favorite: formData.get("favorite") === "true",
+  });
+}
 
 export async function loader({ params }) {
   return getContact(params.contactId);
@@ -67,9 +74,10 @@ export default function Contact() {
 
 function Favorite({ contact }) {
   // yes, this is a `let` for later
+  const fetcher = useFetcher();
   let favorite = contact.favorite;
   return (
-    <Form method="post">
+    <fetcher.Form method="post">
       <button
         name="favorite"
         value={favorite ? "false" : "true"}
@@ -77,6 +85,6 @@ function Favorite({ contact }) {
       >
         {favorite ? "★" : "☆"}
       </button>
-    </Form>
+    </fetcher.Form>
   );
 }
